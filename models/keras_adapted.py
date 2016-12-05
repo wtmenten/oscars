75396388312c3b362ce07b012c3ae4775bb6248c
William Menten-Weil <wtmenten@gmail.com>
2016-12-05 15:48:35 -0800

    added analysis tests

--- models/keras_adapted.py
+++ models/keras_adapted.py
@@ -24,6 +24,7 @@ if cmd_subfolder not in sys.path:
 rcParams['figure.figsize'] = [9, 6]
 
 seed = 7
+# np.random.seed(seed)
 
 
 def shuffle_in_unison(a, b):
@@ -53,6 +54,10 @@ def _no_group_data():
     Y = encoder_r.transform(Y)
     X[:, 1] = encoder_p.transform(X[:, 1])
     X[:, 0] = encoder_y.transform(X[:, 0])
+    X_scaler = StandardScaler()
+    X = np.array(X_scaler.fit_transform(X.astype(float)))
+    X, Y = shuffle_in_unison(X,Y)
+    X = X.astype(float)
     return X, Y
 
 def _group_data():
@@ -84,7 +89,7 @@ def _group_data():
     feature_len = len(fields[1:])
     # print feature_len
     np.zeros_like((seasons, maxSeasonlen))
-    out = (np.zeros((seasons, maxSeasonlen, feature_len)),np.zeros((seasons, maxSeasonlen)))
+    out = [np.zeros((seasons, maxSeasonlen, feature_len)),np.zeros((seasons, maxSeasonlen))]
     # print out[0][1]
     for index, (season_year, group_indexes) in enumerate(g):
         season = [field_subset.iloc[i].values for i in group_indexes]
@@ -97,6 +102,10 @@ def _group_data():
         out[1][index][:len(y)] = y
         # shuffles the season so that there is no bias against the later channels which are sparse more often
         out[0][index], out[1][index] = shuffle_in_unison(out[0][index],out[1][index])
+    X_scaler = StandardScaler()
+    out[0] = np.array([X_scaler.fit_transform(x) for x in out[0]])
+    out[0], out[1] = shuffle_in_unison(out[0], out[1])
+    out[0] = out[0].astype(float)
     return out[0], out[1]
 
 
@@ -132,15 +141,15 @@ def create_larger():
     return model
 
 def create_grouped():
-    np.random.seed(seed)
+    # np.random.seed(seed)
 
     feature_len = len(X[0, 0,:])
     season_len = len(X[0,:])
     model = Sequential()
     node_stretch = 8
-    model.add(GaussianNoise(.20,input_shape=(season_len,feature_len)))
+    model.add(GaussianNoise(.05,input_shape=(season_len,feature_len)))
     # model.add(Flatten(input_shape=(season_len,feature_len)))
-    model.add(Convolution1D(1, 1,input_shape=(season_len,feature_len), input_length=feature_len,bias=True))
+    model.add(Convolution1D(64, 1,input_shape=(season_len,feature_len), input_length=feature_len,bias=True))
 
     model.add(Flatten())
 
@@ -153,12 +162,12 @@ def create_grouped():
     model.add(Dense(int(feature_len *.20 * node_stretch), init='normal', activation='tanh'))
     model.add(Dropout(.2))
     # model.add(Dense(1, init='normal', activation='sigmoid'))
-    model.add(Dense(12, init='normal', activation='sigmoid'))
+    # model.add(Dense(12, init='normal', activation='sigmoid'))
     model.add(Dense(12, init='normal', activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'precision', 'recall', 'fmeasure'])
     return model
 
-def create_mergegrouped():
+def create_mergegrouped(base_params):
     # np.random.seed(seed)
 
     feature_len = len(X[0, 0, :])
@@ -167,13 +176,14 @@ def create_mergegrouped():
 
     l1_size = int(feature_len * 1 * node_stretch)
     l2_size = int(feature_len * 1 * node_stretch)
-    noise_std = .1
+    noise_std = base_params['noise']
+    conv_size = base_params['conv_size']
     left_model = Sequential()
     right_model = Sequential()
     left_model.add(GaussianNoise(noise_std, input_shape=(season_len, feature_len)))
     right_model.add(GaussianNoise(noise_std, input_shape=(season_len, feature_len)))
-    left_model.add(Convolution1D(1, 1, input_shape=(season_len, feature_len), input_length=feature_len))
-    right_model.add(Convolution1D(1, 1, input_shape=(season_len, feature_len), input_length=feature_len))
+    left_model.add(Convolution1D(conv_size, 1, input_shape=(season_len, feature_len), input_length=feature_len))
+    right_model.add(Convolution1D(conv_size, 1, input_shape=(season_len, feature_len), input_length=feature_len))
     left_model.add(Flatten())
     right_model.add(Flatten())
 
@@ -186,13 +196,12 @@ def create_mergegrouped():
     final_model = Sequential()
     merged = Merge([left_model,right_model], mode='ave')
     final_model.add(merged)
-    final_model.add(Dense(l2_size, init='normal'))
-    final_model.add(Activation('sigmoid'))
+    final_model.add(Dense(l2_size, init='normal', activation='sigmoid'))
     # final_model.add(PReLU())
     # final_model.add(LeakyReLU())
     final_model.add(Dropout(.1))
     final_model.add(Dense(12, init='normal', activation='softmax',bias=True))
-    final_model.compile(loss='categorical_crossentropy', optimizer='adagrad',
+    final_model.compile(loss='categorical_crossentropy', optimizer='adam',
                   metrics=['accuracy', 'precision', 'recall', 'fmeasure'])
     return final_model
 
@@ -234,123 +243,130 @@ def make_plots(test, preds, acc, loss, trial):
     plt.savefig('tests/graphs/loss_%s.png' % trial)
     plt.close(fig)
 
-def _score(scores, model, include=True):
-    accs.append(scores[1] * 100)
-    precs.append(scores[2] * 100)
-    recs.append(scores[3] * 100)
-    fscores.append(scores[4] * 100)
-    print("_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-    print(scores, model.metrics_names)
+def _score(scores, model, test=False):
+    if not test:
+        accs.append(scores[1] * 100)
+        precs.append(scores[2] * 100)
+        recs.append(scores[3] * 100)
+        fscores.append(scores[4] * 100)
+        trained_models.append(model)
+        print("_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+        print(scores, model.metrics_names)
+    else:
+        print("_-_-_-_-_Test Scores_-_-_-_-_")
+        print("Acc.  %s" % (scores[1] * 100))
+        print("prec.  %s" % (scores[2] * 100))
+        print("rec.  %s" % (scores[3] * 100))
+        print("F.  %s" % (scores[4] * 100))
 
-def test_fold(train, test, trial, lasttest=None):
-    X_scaler = StandardScaler()
-    X_train = X_scaler.fit_transform(X[train])
-    X_test = X_scaler.transform(X[test])
-    hist = model.fit(X_train, Y[train], nb_epoch=750, batch_size=50, verbose=0)
-    # if lasttest is not None:
-        # X_train = X_scaler.fit_transform(X[lasttest])
-        # hist = model.fit(X_train, Y[lasttest], nb_epoch=750, batch_size=50, verbose=0)
-    preds = model.predict_classes(X_test, batch_size=50, verbose=0)
-    scores = model.evaluate(X_test, Y[test], verbose=0)
-    accs.append(scores[1] * 100)
-    precs.append(scores[2] * 100)
-    recs.append(scores[3] * 100)
-    fscores.append(scores[4] * 100)
-    print("_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-    print(scores, model.metrics_names)
-    #     print(hist.history.keys())
-    #     print(preds)
+def train_nogroup(train, test, trial):
+    hist = model.fit(train[0], train[1], nb_epoch=750, batch_size=50, verbose=0)
+    preds = model.predict_classes(train[0], batch_size=50, verbose=0)
+    scores = model.evaluate(test[0], test[1], verbose=0)
+    _score(scores,model)
     make_plots(test, preds, hist.history['acc'], hist.history['loss'], trial)
-    return test
 
-def train_group(train, vaild, trial):
-    np.random.seed(seed)
-
-    X_train = np.array([x for x in train[0]])
-    X_valid = np.array([x for x in vaild[0]])
-    hist = model.fit(X_train, train[1], nb_epoch=500, batch_size=10, verbose=0)
-    preds = model.predict_classes(X_valid, batch_size=10, verbose=0)
-    # preds = model.predict(X_test, batch_size=50, verbose=0)
-    scores = model.evaluate(X_valid, vaild[1], verbose=0)
-    _score(scores,model)
-    make_plots(vaild[1], preds, hist.history['acc'], hist.history['loss'], trial)
-
-def train_mergegroup(train, vaild, trial):
+def train_group(train, valid, trial):
     # np.random.seed(seed)
-
-    X_train = np.array([x for x in train[0]])
-    X_valid = np.array([x for x in vaild[0]])
-    hist = model.fit([X_train,X_train], train[1], nb_epoch=200, batch_size=10, verbose=0)
-    preds = model.predict_classes([X_valid,X_valid], batch_size=10, verbose=0)
+    hist = model.fit(train[0], train[1], nb_epoch=500, batch_size=10, verbose=0)
+    preds = model.predict_classes(valid[0], batch_size=10, verbose=0)
     # preds = model.predict(X_test, batch_size=50, verbose=0)
-    scores = model.evaluate([X_valid,X_valid], vaild[1], verbose=0)
+    scores = model.evaluate(valid[1], vaild[1], verbose=0)
     _score(scores,model)
     make_plots(vaild[1], preds, hist.history['acc'], hist.history['loss'], trial)
+
+def train_mergegroup(train, valid, trial):
+    # np.random.seed(seed)
+    hist = model.fit([train[0],train[0]], train[1], nb_epoch=600, batch_size=10, verbose=0)
+    preds = model.predict_classes([valid[0],valid[0]], batch_size=10, verbose=0)
+    scores = model.evaluate([valid[0],valid[0]], valid[1], verbose=0)
+    _score(scores,model)
+    make_plots(vaild[1], preds, hist.history['acc'], hist.history['loss'], trial)
+
+
+def test_nogroup(test):
+    preds = model.predict_classes(test[0], batch_size=50, verbose=0)
+    scores = model.evaluate(test[0], test[1], verbose=0)
+    _score(scores,model, test=True)
+    # make_plots(test, preds, hist.history['acc'], hist.history['loss'], trial)
 
 def test_group(test):
-    np.random.seed(seed)
+    # np.random.seed(seed)
 
     preds = model.predict_classes(test[0], batch_size=10, verbose=0)
     # preds = model.predict(X_test, batch_size=50, verbose=0)
     scores = model.evaluate(test[0], test[1], verbose=0)
-    _score(scores,model, include=False)
-    print(preds)
-    print(test[1])
+    _score(scores,model, test=True)
     # make_plots(vaild[1], preds, hist.history['acc'], hist.history['loss'], 0)
 
 def test_mergegroup(test):
     # np.random.seed(seed)
 
     preds = model.predict_classes([test[0],test[0]], batch_size=10, verbose=0)
-    # preds = model.predict(X_test, batch_size=50, verbose=0)
     scores = model.evaluate([test[0],test[0]], test[1], verbose=0)
-    _score(scores, model, include=False)
-    # print(preds)
-    # print(test[1])
+    _score(scores, model, test=True)
     # make_plots(vaild[1], preds, hist.history['acc'], hist.history['loss'], 0)
 # print X[0]
 # print Y[0]
-# assert 1 ==0
 # np.random.seed(seed)
 X,Y = _group_data()
-X_scaler = StandardScaler()
-X = np.array([X_scaler.fit_transform(x) for x in X])
-X,Y = shuffle_in_unison(X,Y)
-X = X.astype(float)
+# X,Y = _no_group_data()
 split_point = int(len(X)*.8)
 x_train, x_test = [X[:split_point],X[split_point:]]
 y_train, y_test = [Y[:split_point],Y[split_point:]]
 # kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-kfold = KFold(n_splits=2, shuffle=True)
+kfold = KFold(n_splits=5, shuffle=True,random_state=seed)
 trial = 1
 accs = []
 precs = []
 recs = []
 fscores = []
-model = create_mergegrouped()
-print model.layers[0].output_shape
-print model.layers[1].output_shape
+
+
 # print model.layers[2].output_shape
 folds = list(kfold.split(x_train, y_train))
-# print Y[folds[0][1]]
-# print Y[folds[1][1]]
 # assert 1 ==0
 # test_group((X[folds[0][0]],Y[folds[0][0]]),(X[folds[0][1]],Y[folds[0][1]]),trial)
 # test_group((X[:-2],Y[:-2]),(X[-2:],Y[-2:]),trial)
 # trial += 1
 # test_fold(folds[1][0],folds[1][1],trial)
 # trial += 1
-lasttest = None
-for train, vaild in folds:
+p_scale = 15
+p_const = 1
+p_name = 'conv_size'
+param_values = [p_const+x*p_scale for x in range(len(folds))]
+trained_models = []
+for (train, vaild), param_val in zip(folds,param_values):
+    base_params = {
+        'noise': .03,
+        'conv_size': 120,
+    }
+    # base_params[p_name] = param_val
+    # print('training %s with value %s' % (p_name,param_val))
+
+    # model = create_larger()
+    model = create_mergegrouped(base_params)
+    # model = create_grouped()
+
+    # for l in model.layers:
+    #     print(l.output_shape)
+    # train_nogroup((x_train[train],y_train[train]),(x_train[vaild],y_train[vaild]),trial)
     train_mergegroup((x_train[train],y_train[train]),(x_train[vaild],y_train[vaild]),trial)
     # train_group((x_train[train],y_train[train]),(x_train[vaild],y_train[vaild]),trial)
-    # lasttest = test_fold(train, test,trial, lasttest=lasttest)
+
     trial += 1
+selection_list = [[f,m]for f,m in zip(fscores,trained_models)]
+selection_list.sort(key=lambda x: x[0], reverse=True)
+model = selection_list[0][1]
+print('best model had fscore %s' % selection_list[0][0])
+# test_group((x_test,y_test))
 test_mergegroup((x_test,y_test))
+# test_nogroup((x_test,y_test))
+
 
 for i, scs in enumerate(zip(accs, precs, recs, fscores)):
-    print('Stat report for round %s: Acc. %.2f%% | Prec. %.2f%% | Rec. %.2f%% | Fscore %.2f%%' % (i + 1, scs[0], scs[1], scs[2], scs[3]))
-print('Average accuracy over all rounds: %.2f%% (+/- %.2f%%)' % (np.mean(accs), np.std(accs)))
-print('Average precision over all rounds: %.2f%% (+/- %.2f%%)' % (np.mean(precs), np.std(precs)))
-print('Average recall over all rounds: %.2f%% (+/- %.2f%%)' % (np.mean(recs), np.std(recs)))
-print('Average Fscore over all rounds: %.2f%% (+/- %.2f%%)' % (np.mean(fscores), np.std(fscores)))
+    print('Stat report for fold %s: Acc. %.2f%% | Prec. %.2f%% | Rec. %.2f%% | Fscore %.2f%%' % (i + 1, scs[0], scs[1], scs[2], scs[3]))
+print('Average accuracy over all folds: %.2f%% (+/- %.2f%%)' % (np.mean(accs), np.std(accs)))
+print('Average precision over all folds: %.2f%% (+/- %.2f%%)' % (np.mean(precs), np.std(precs)))
+print('Average recall over all folds: %.2f%% (+/- %.2f%%)' % (np.mean(recs), np.std(recs)))
+print('Average Fscore over all folds: %.2f%% (+/- %.2f%%)' % (np.mean(fscores), np.std(fscores)))
