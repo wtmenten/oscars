294c4b0933437e71dde0ab01edee7c426e1c266a
Brooks Lobe <brooks.lobe@gmail.com>
2016-12-09 17:13:24 -0800

    added folder with website contents

---
+++ website/js/custom.js
@@ -0,0 +1,22 @@
+// Offset for Site Navigation
+$('#siteNav').affix({
+	offset: {
+		top: 100
+	}
+})
+
+//Easing
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
